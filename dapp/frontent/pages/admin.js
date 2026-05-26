d07d276b1b6c438ca881ee7359ea1ac0d83de46d
Miguel <miguelou04@gmail.com>
2026-05-26 16:19:11 +0100

    some work

--- dapp/frontent/pages/admin.js
+++ dapp/frontent/pages/admin.js
@@ -8,7 +8,7 @@ const DEX_ABI = [
   "function owner() external view returns (address)"
 ];
 
-const DEX_ADDRESS = "xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+const DEX_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 export default function AdminConsole() {
   const [rate, setRate] = useState('');
--- dapp/scripts/deploy.js
+++ dapp/scripts/deploy.js
@@ -6,28 +6,28 @@ async function main() {
   const [deployer] = await hre.ethers.getSigners();
   console.log("⚡ A publicar contratos com a conta:", deployer.address);
 
-  // 1. Deploy do DEX (Com os TEUS parâmetros originais)
+  
   const DecentralizedFinance = await hre.ethers.getContractFactory("DecentralizedFinance");
   const dex = await DecentralizedFinance.deploy(1000, 60, 10, hre.ethers.parseEther("0.01"), 86400);
   
   await dex.waitForDeployment();
   const dexAddress = await dex.getAddress();
-  console.log("🟢 Contrato DEX publicado no endereço:", dexAddress);
+  console.log("Contrato DEX publicado no endereço:", dexAddress);
 
-  // 2. Deploy do NFT Market
+  //  Deploy do NFT Market
   const NFTPawningMarketplace = await hre.ethers.getContractFactory("NFTPawningMarketplace");
   const nftMarket = await NFTPawningMarketplace.deploy(dexAddress);
   
   await nftMarket.waitForDeployment();
   const nftMarketAddress = await nftMarket.getAddress();
-  console.log("🟢 Contrato NFT publicado no endereço:", nftMarketAddress);
+  console.log(" Contrato NFT publicado no endereço:", nftMarketAddress);
 
   // 3. EXPORTAR PARA O BACKEND (A Mágica da Web2.5)
   saveToBackend(dexAddress, nftMarketAddress);
 }
 
 function saveToBackend(dexAddr, nftAddr) {
-  // Guarda o ficheiro na raiz do projeto, ao lado do server.js
+ 
   const backendConfigPath = path.join(__dirname, "..", "backend", "contractConfig.json");
   const dexArtifact = hre.artifacts.readArtifactSync("DecentralizedFinance");
   const nftMarketArtifact = hre.artifacts.readArtifactSync("NFTPawningMarketplace");
